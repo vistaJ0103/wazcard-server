fa2e8d95fc731b7cdf0ccbb8a166b163870cdf36
vista <vista0103@outlook.com>
2024-05-15 19:26:01 +0900

    fix:bugs

--- models/card_views.js
+++ models/card_views.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class card_views extends Model {
     /**
@@ -10,35 +8,38 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      card_views.belongsTo(models.cards, { foreignKey: "cid" })
+      card_views.belongsTo(models.cards, { foreignKey: "cid" });
     }
   }
-  card_views.init({
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER,
+  card_views.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      cid: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      countryName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
     },
-    cid: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    countryName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-  }, {
-    sequelize,
-    modelName: 'card_views',
-  });
+    {
+      sequelize,
+      modelName: "card_views",
+    }
+  );
   return card_views;
 };
